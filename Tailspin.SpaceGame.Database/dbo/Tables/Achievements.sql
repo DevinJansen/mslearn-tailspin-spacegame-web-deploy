c865f5d6d877e1544cce1f27f1b81c5b28ef15fa
Devin Jansen <devin@gomazevo.com>
2020-09-10 09:18:20 -0600

    Adding Created date to Achivements

--- Tailspin.SpaceGame.Database/dbo/Tables/Achievements.sql
+++ Tailspin.SpaceGame.Database/dbo/Tables/Achievements.sql
@@ -1,5 +1,6 @@
 ﻿CREATE TABLE [dbo].[Achievements] (
     [id]            INT           NOT NULL,
     [description]   NVARCHAR (50)    NULL,
+    [createdDate]   DATETIME        NULL,
     PRIMARY KEY CLUSTERED ([id] ASC)
 );
